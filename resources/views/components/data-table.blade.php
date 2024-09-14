354d4af3f57a548dc793e0def8cc56cb075b384c
na0260 <samjr.008@gmail.com>
2024-09-15 02:50:40 +0600

    datatable component added

---
+++ resources/views/components/data-table.blade.php
@@ -0,0 +1,11 @@
+<table class="table table-bordered" id="productTable">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+</table>
