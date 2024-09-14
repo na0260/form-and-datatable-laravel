2082671e4b723a90b287447c727c35c27e3088ed
na0260 <samjr.008@gmail.com>
2024-09-15 02:53:19 +0600

    product List ui created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="container">
+                        <div class="d-flex justify-content-between align-items-center mb-4">
+                            <h1 class="font-bold">Product List</h1>
+                            <button class="btn btn-primary" id="addNewBtn">Add New</button>
+                        </div>
+
+                        <!-- Add/Edit Product Modal -->
+                        <x-form-modal />
+
+                        <!-- Products Table -->
+                        <x-data-table />
+                    </div>
                 </div>
             </div>
         </div>
