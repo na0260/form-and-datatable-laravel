b5229023c9bb2f018270daee94b308d029e61de3
na0260 <samjr.008@gmail.com>
2024-09-15 03:36:32 +0600

    index and get product function added

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('dashboard');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Get the products data.
+     */
+    public function getProductsData(){
+        $products = Product::select(['id', 'name', 'price', 'quantity']);
+
+        return DataTables::of($products)
+            ->addColumn('actions', function($row) {
+                return '
+                    <button class="btn btn-sm btn-primary editBtn" data-id="'.$row->id.'">Edit</button>
+                    <button class="btn btn-sm btn-danger deleteBtn" data-id="'.$row->id.'">Delete</button>
+                ';
+            })
+            ->rawColumns(['actions'])  // Allow HTML rendering in actions column
+            ->make(true);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
